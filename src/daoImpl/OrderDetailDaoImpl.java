54b7a6f11d7d7cb66618e69c8e1d70c57ad234d7
abdullahbyrmv <abdullah.bayramov@inbox.ru>
2023-11-27 17:20:35 +0400

    Update

--- src/daoImpl/OrderDetailDaoImpl.java
+++ src/daoImpl/OrderDetailDaoImpl.java
@@ -1,8 +1,11 @@
 package daoImpl;
 
 import abstractDao.AbstractDao;
+import dao.BookInterface;
 import dao.OrderDetailInterface;
+import entity.Book;
 import entity.OrderDetail;
+import main.Context;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -15,11 +18,37 @@ public class OrderDetailDaoImpl extends AbstractDao implements OrderDetailInterf
 
     @Override
     public boolean addOrderDetail(OrderDetail orderDetail) {
-        try (Connection connection = connect();) {
-            PreparedStatement st = connection.prepareStatement("INSERT INTO order_detail (order_id,book_id) VALUES (?,?)");
-            st.setInt(1, orderDetail.getOrder_id());
-            st.setInt(2, orderDetail.getBook_id());
-            return st.execute();
+
+        BookInterface bookInterface = Context.instanceBookDao();
+
+        try (Connection connection = connect();
+             PreparedStatement insertOrderStatement = connection.prepareStatement("INSERT INTO order_detail (order_id, book_id,number_of_books) VALUES (?, ?, ?)");
+             PreparedStatement updateBookStatement = connection.prepareStatement("UPDATE book SET stock = ? WHERE book_id = ?")) {
+
+            connection.setAutoCommit(false);
+
+            int requestedBookId = orderDetail.getBook_id();
+            int requestedQuantity = orderDetail.getNumber_of_books();
+            Book book = bookInterface.getBookById(requestedBookId);
+
+            if (book != null && book.getStock() >= requestedQuantity) {
+                insertOrderStatement.setInt(1, orderDetail.getOrder_id());
+                insertOrderStatement.setInt(2, orderDetail.getBook_id());
+                insertOrderStatement.setInt(3, orderDetail.getNumber_of_books());
+                insertOrderStatement.executeUpdate();
+
+                int updatedStock = book.getStock() - requestedQuantity;
+                updateBookStatement.setInt(1, updatedStock);
+                updateBookStatement.setInt(2, requestedBookId);
+                updateBookStatement.executeUpdate();
+
+                connection.commit();
+                return true;
+            } else {
+                connection.rollback();
+                System.out.println("Not enough books in stock for this order.");
+                return false;
+            }
         } catch (Exception e) {
             System.out.println("An error occurred: " + e.getMessage());
             return false;
@@ -36,7 +65,8 @@ public class OrderDetailDaoImpl extends AbstractDao implements OrderDetailInterf
             while (res.next()) {
                 int order_id = res.getInt("order_id");
                 int book_id = res.getInt("book_id");
-                orderDetailList.add(new OrderDetail(order_id, book_id));
+                int number_of_books = res.getInt("number_of_books");
+                orderDetailList.add(new OrderDetail(order_id, book_id, number_of_books));
             }
         } catch (Exception e) {
             System.out.println("An error occurred: " + e.getMessage());
@@ -47,9 +77,10 @@ public class OrderDetailDaoImpl extends AbstractDao implements OrderDetailInterf
     @Override
     public boolean updateOrderDetail(OrderDetail orderDetail) {
         try (Connection connection = connect()) {
-            PreparedStatement st = connection.prepareStatement("UPDATE order_detail SET book_id=? WHERE order_id=?");
+            PreparedStatement st = connection.prepareStatement("UPDATE order_detail SET book_id=?,number_of_books=? WHERE order_id=?");
             st.setInt(1, orderDetail.getBook_id());
-            st.setInt(2, orderDetail.getOrder_id());
+            st.setInt(2, orderDetail.getNumber_of_books());
+            st.setInt(3, orderDetail.getOrder_id());
             return st.execute();
         } catch (Exception e) {
             System.out.println("An error occurred: " + e.getMessage());
@@ -79,7 +110,8 @@ public class OrderDetailDaoImpl extends AbstractDao implements OrderDetailInterf
             while (res.next()) {
                 int id_order = res.getInt("order_id");
                 int id_book = res.getInt("book_id");
-                orderDetail = new OrderDetail(id_order, id_book);
+                int number_of_books = res.getInt("number_of_books");
+                orderDetail = new OrderDetail(id_order, id_book, number_of_books);
             }
         } catch (Exception e) {
             System.out.println("An error occurred: " + e.getMessage());
--- src/entity/OrderDetail.java
+++ src/entity/OrderDetail.java
@@ -3,10 +3,12 @@ package entity;
 public class OrderDetail {
     private int order_id;
     private int book_id;
+    private int number_of_books;
 
-    public OrderDetail(int order_id, int book_id) {
+    public OrderDetail(int order_id, int book_id, int number_of_books) {
         this.order_id = order_id;
         this.book_id = book_id;
+        this.number_of_books = number_of_books;
     }
 
     public OrderDetail() {
@@ -28,11 +30,20 @@ public class OrderDetail {
         this.book_id = book_id;
     }
 
+    public int getNumber_of_books() {
+        return number_of_books;
+    }
+
+    public void setNumber_of_books(int number_of_books) {
+        this.number_of_books = number_of_books;
+    }
+
     @Override
     public String toString() {
         return "OrderDetail{" +
                 "order_id=" + order_id +
                 ", book_id=" + book_id +
+                ", number_of_books=" + number_of_books +
                 '}';
     }
 }
